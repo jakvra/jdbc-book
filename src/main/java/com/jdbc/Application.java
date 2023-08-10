5d22dcd45e0823a5ea05fd86b36b33050820fe0c
Jakub Vrana <vrana.jakub@feg.eu>
2023-08-11 01:32:46 +0200

    Exercise 1, 2 - adding HSQLDB - and use it as file-based database

--- src/main/java/com/jdbc/Application.java
+++ src/main/java/com/jdbc/Application.java
@@ -1,33 +1,25 @@
 package com.jdbc;
 
-import com.zaxxer.hikari.HikariDataSource;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        DataSource dataSource = createDataSource();
+        // Creating the connection with HSQLDB
+        String db = "jdbc:hsqldb:file:~/marco/hsqldb";
+        String user = "root";
+        String password = "password";
 
-        try (Connection connection = dataSource.getConnection()) {
-
+        try (Connection connection = DriverManager.getConnection(db, user, password)) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
-    }
-
-    private static DataSource createDataSource() {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl("jdbc:h2:~/mydatabase");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("somePassword");
-        return dataSource;
     }
 
 }
