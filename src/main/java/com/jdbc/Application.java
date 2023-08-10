d168f515313cd7775ef5435e396e294b9bc7cddb
Jakub Vrana <vrana.jakub@feg.eu>
2023-08-10 22:04:30 +0200

    2-database-connections.mp4

--- src/main/java/com/jdbc/Application.java
+++ src/main/java/com/jdbc/Application.java
@@ -1,8 +1,20 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // create and connect to the H2 in memory database
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+
+            System.out.println("connection.isValid(0) = " + connection.isValid(0));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
